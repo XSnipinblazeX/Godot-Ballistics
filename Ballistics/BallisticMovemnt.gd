0de58cc03c39cb5b739f7e9800dc06700b273af3
XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>
2024-07-28 08:27:58 -0700

    Update BallisticMovemnt.gd
    Made code a tad more readable Move all the movement math into the move function which returns a new velocity
    
    Signed-off-by: XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>

--- Ballistics/BallisticMovemnt.gd
+++ Ballistics/BallisticMovemnt.gd
@@ -1,65 +1,75 @@
 # Made by Aj8841 @ xsnipinblazex@gmail.com 
 
-#This is very basic Ballistic trajectory with a *not* working hit detection
-#simulates gravity, drag, and the magnus effect
-# this is all I deem necessary for a realistic trajectory for all types of projectiles. Magnus effect simulates magnus and possibly spin drift, haven't seen it yet since I'm making this on godot mobile
-# about 4/5 accurate when compared to my references using the F5 Sports Pitchlogic System
+# This is a very basic Ballistic trajectory with a *not* working hit detection
+# Simulates gravity, drag, and the magnus effect
+# This is all I deem necessary for a realistic trajectory for all types of projectiles.
+# Magnus effect simulates magnus and possibly spin drift, haven't seen it yet since I'm making this on Godot mobile
+# About 4/5 accurate when compared to my references using the F5 Sports Pitchlogic System
+# Updated 7/28/2024
 
-#updated 7/28/2024
+extends MeshInstance3D # Extends any node as long as it doesn't affect the gravity and velocity
 
+var mass = 0.0167 # Mass of the object in kg
+var Active = false # For future initialization
+var iV = 39 # iV: Initial velocity in m/s
+var velocity = Vector3(0, 0, 0) # This is the actual velocity during runtime
+var direction = Vector3(0, -0.04, -1) # Direction of travel degrees/90
+var spin = Vector3(1252, 1480, -858) # Spin rate in rpm (sidespin +(CW), backspin +(CCW), riflespin +(CW)) *see commit on GitHub for more info
 
-extends MeshInstance3D # extends any node as long as it doesn't affect the gravity and velocity
-
-var mass = 0.0167 #mass of the object In kg
-
-var Active = false # for future initialization
-
-var iV = 39.294816 # 87.9 mph iV: Initial velocity in m/s
-var velocity = Vector3(0, 0, 0) # this is the actual velocity during runtime
-var direction = Vector3(0, -0.04, -1) # direction of travel degrees/90
-var spin = Vector3(1252, 1480, -858) # spin rate in rpm (sidespin +(CW), backspin +(CCW), riflespin +(CW)) *see commit on github for more infoaa
-
-var angDamp = 0.2 # rotational drag Coefficient 
-var linDamp = 0.35 # linear drag Coefficient 
-var Dia = 0.075 # diameter of object 
-var area = 0.01723 # cross sectional area
+var angDamp = 0.2 # Rotational drag coefficient 
+var linDamp = 0.35 # Linear drag coefficient 
+var Dia = 0.075 # Diameter of object 
+var area = 0.01723 # Cross-sectional area
 
 var Air_density = 1.225 
-const c = 1 # a constant for scaling if needed
-var rps = 2 * PI / 60 # rpm to radians a second conversion
-var GRAV = -9.81 # acceleration of gravity in m/s
-var MoI = 0.09  # moment of inertia 
-var airRes = 0.5 * Air_density * area #air resistance factor
+const c = 1 # A constant for scaling if needed
+var rps = 2 * PI / 60 # RPM to radians a second conversion
+var GRAV = -9.81 # Acceleration of gravity in m/s
+var MoI = 0.09  # Moment of inertia 
+var airRes = 0.5 * Air_density * area # Air resistance factor
 
 var raycast
-var lastPos = Vector3()
+var lastPos = global_transform.origin 
 
 func _ready():
-    velocity = iV * direction # fires in direction with velocoty
-    # spin *= Vector3(-1, 1, -1) # makes spin relative to F5 Sports’ pitchlogic system
+    velocity = iV * direction # Fires in direction with velocity
+    # spin *= Vector3(-1, 1, -1) # Makes spin relative to F5 Sports’ Pitchlogic system
     Active = true
 
     # Get the RayCast node
     raycast = $RayCast
     raycast.enabled = true
 
+func Move(pos, _velocity, delta, returnNewPos, force):
+    # Accelerate to gravity
+    _velocity.y += GRAV * delta * c 
+    
+    var drag_force = airRes * _velocity * _velocity * linDamp # Linear drag
+    _velocity -= drag_force * delta
+    
+    var magnus_force = (spin * rps) * (Dia / 2) / (mass * (_velocity.length() * 2))  # Get the force of the magnus effect
+    var AngDrag = -angDamp * spin * (Dia / 2) 
+    var torque = -(Dia / 2) * AngDrag
+    if spin.length() < 0:
+        torque *= -1
+    var angAccel = torque / MoI # Angular drag
+    spin -= angAccel
+    _velocity += magnus_force * delta
+    
+    return _velocity
+
+var currentPos = Vector3()
+var nextPos = Vector3()
+
 func _physics_process(delta):
     if Active:
-        lastPos = global_transform.origin
-        velocity.y += GRAV * delta * c # accelerate to gravity
-        var spinforce = (spin * rps) * (Dia / 2) / (mass * (velocity.length() * 2))  # Get the force of the magnus effect
-        var AngDrag = -angDamp * spin * (Dia / 2) 
-        var torque = -(Dia / 2) * AngDrag
-        if spin.length() < 0:
-            torque *= -1
-        var angAccel = torque / MoI # angular drag
-        spin -= angAccel
-        var DF = airRes * velocity * velocity * linDamp #linear drag
-        velocity -= DF * delta
-        velocity += spinforce * delta
-
-        # Update the position of the ball
+        velocity = Move(lastPos, velocity, delta, false, true)
         global_transform.origin += velocity * delta
+        currentPos = global_transform.origin
+        nextPos = currentPos * Move(currentPos, velocity, delta, true, false) * delta # This should allow for hit detection because this is a frame ahead
+        
+        currentPos = global_transform.origin
+        
+        lastPos = currentPos
 
-
-        #do hit check here
+        # Do hit check here
