279df0e10ea23115427210e000dd323ad6868a9a
XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>
2024-07-29 10:50:41 -0700

    Update BallisticMovemnt.gd
    Changed graph to change in velocity over time currently working on ways to use that but it works regardless
    
    Signed-off-by: XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>

--- Ballistics/BallisticMovemnt.gd
+++ Ballistics/BallisticMovemnt.gd
@@ -7,14 +7,14 @@
 # About 4/5 accurate when compared to my references using the F5 Sports Pitchlogic System
 # Updated 7/28/2024
 # Updated 7/29/24 10:37AM PST
-
+# Updated 7/29/24 10:49AM PST
 
 extends MeshInstance3D
 
 var mass = 0.0167
 var Active = false
 var iV = 39
-var velocity = Vector3(0, 0, 0)
+var initial_velocity = Vector3()
 var direction = Vector3(0, -0.04, -1)
 var spin = Vector3(1252, 1480, -858)
 var angDamp = 0.2
@@ -31,30 +31,34 @@ var airRes = 0.5 * Air_density * area
 var lastPos = global_transform.origin
 var current_time = 0.0  # Initialize current time
 
-# Pre-calculated velocity data
-var velocity_data = []
+# Pre-calculated change in velocity data
+var delta_velocity_data = []
 var max_time = 5.0
 var time_step = 0.1
 
 func _ready():
-    velocity = iV * direction
+    initial_velocity = iV * direction
     Active = true
-    compute_velocities()
+    compute_delta_velocities()
 
-func compute_velocities():
-    var current_velocity = velocity
+func compute_delta_velocities():
+    var current_velocity = initial_velocity
     for t in range(0, max_time / time_step):
         var delta_time = t * time_step
+        var previous_velocity = current_velocity
         current_velocity = Move(lastPos, current_velocity, time_step)
-        velocity_data.append({
+        
+        # Calculate change in velocity (stored as negative for forces adding to velocity)
+        var delta_velocity = previous_velocity - current_velocity
+        delta_velocity_data.append({
             "time": delta_time,
-            "velocity": current_velocity
+            "delta_velocity": delta_velocity
         })
 
-func get_velocity_at_time(target_time):
-    for data in velocity_data:
+func get_delta_velocity_at_time(target_time):
+    for data in delta_velocity_data:
         if data["time"] >= target_time:
-            return data["velocity"]
+            return data["delta_velocity"]
     return null
 
 func Move(pos, _velocity, delta):
@@ -69,15 +73,17 @@ func Move(pos, _velocity, delta):
         torque *= -1
     var angAccel = torque / MoI
     spin -= angAccel
-    _velocity += magnus_force * delta
+    _velocity += magnus_force * delta  # Update velocity with Magnus force
     
     return _velocity
 
 func _physics_process(delta):
     if Active:
         current_time += delta  # Increment current time by delta
-        var velocity_at_time = get_velocity_at_time(current_time)
+        var delta_velocity_at_time = get_delta_velocity_at_time(current_time)
         
-        if velocity_at_time:
-            global_transform.origin += velocity_at_time * delta
+        if delta_velocity_at_time:
+            # Update the velocity by subtracting the change in velocity
+            
+            global_transform.origin += (initial_velocity - delta_velocity_at_time) * delta
             lastPos = global_transform.origin
