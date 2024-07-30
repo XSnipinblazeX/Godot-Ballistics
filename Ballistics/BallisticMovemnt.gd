12b90e800176c402e6441eae488994172a92e004
XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>
2024-07-29 18:49:18 -0700

    Update BallisticMovemnt.gd
    Signed-off-by: XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>

--- Ballistics/BallisticMovemnt.gd
+++ Ballistics/BallisticMovemnt.gd
@@ -27,93 +27,91 @@ var max_time = 5.0
 var time_step = 0.1
 
 func _ready():
-	velocity = iV * direction
-	lastPos = global_transform.origin
-	Active = false
-	compute_delta_velocities()
+    velocity = iV * direction
+    lastPos = global_transform.origin
+    Active = false
+    compute_delta_velocities()
 
 func compute_delta_velocities():
-	var current_velocity = velocity
-	for t in range(0, int(max_time / time_step)):
-		var delta_time = t * time_step
-		var previous_velocity = current_velocity
-		current_velocity = Move(lastPos, current_velocity, delta_time, false, true)
-		
-		# Calculate change in velocity (negative because we want to subtract from initial velocity)
-		var delta_velocity = previous_velocity - current_velocity
-		velocity = current_velocity 
-		
-		# Calculate Magnus force at this time
-		var magnus_force = Magnus(current_velocity)
-		magnus_data.append({
-			"time": delta_time,
-			"magnus_force": magnus_force
-		})
-		
-		delta_velocity_data.append({
-			"time": delta_time,
-			"delta_velocity": delta_velocity
-		})
-	velocity = iV * direction
-	Active = true
+    var current_velocity = velocity
+    for t in range(0, int(max_time / time_step)):
+        var delta_time = t * time_step
+        var previous_velocity = current_velocity
+        current_velocity = Move(lastPos, current_velocity, delta_time, false, true)
+        
+        # Calculate change in velocity (negative because we want to subtract from initial velocity)
+        var delta_velocity = previous_velocity - current_velocity
+        velocity = current_velocity 
+        
+        # Calculate Magnus force at this time
+        var magnus_force = Magnus(current_velocity)
+        magnus_data.append({
+            "time": delta_time,
+            "magnus_force": magnus_force
+        })
+        
+        delta_velocity_data.append({
+            "time": delta_time,
+            "delta_velocity": delta_velocity
+        })
+    velocity = iV * direction
+    Active = true
 
 func get_delta_velocity_at_time(target_time):
-	for data in delta_velocity_data:
-		if data["time"] >= target_time:
-			return data["delta_velocity"]
-	return delta_velocity_data[-1]["delta_velocity"] if delta_velocity_data.size() > 0 else Vector3.ZERO
+    for data in delta_velocity_data:
+        if data["time"] >= target_time:
+            return data["delta_velocity"]
+    return delta_velocity_data[-1]["delta_velocity"] if delta_velocity_data.size() > 0 else Vector3.ZERO
 
 func get_magnus_force_at_time(target_time):
-	for data in magnus_data:
-		if data["time"] >= target_time:
-			return data["magnus_force"]
-	return magnus_data[-1]["magnus_force"] if magnus_data.size() > 0 else Vector3.ZERO
+    for data in magnus_data:
+        if data["time"] >= target_time:
+            return data["magnus_force"]
+    return magnus_data[-1]["magnus_force"] if magnus_data.size() > 0 else Vector3.ZERO
 
 func Magnus(_velocity):
-	var velocity_length = 2 * _velocity.length()
-	if velocity_length > 0:
-		return (spin.cross(_velocity)).normalized() * (spin.length() * rps * Dia / (mass * velocity_length))
-	return Vector3.ZERO
+    var magnus_force = (spin * rps) * (Dia / 2) / (mass * (_velocity.length() * 2))
+    return magnus_force 
 
 func Spin(delta):
-	var angDrag = -angDamp * spin * (Dia / 2)
-	var torque = -(Dia / 2) * angDrag
-	if spin.length() < 0:
-		torque *= -1
-	var angAccel = torque / MoI
-	spin -= angAccel * delta
+    var angDrag = -angDamp * spin * (Dia / 2)
+    var torque = -(Dia / 2) * angDrag
+    if spin.length() < 0:
+        torque *= -1
+    var angAccel = torque / MoI
+    spin -= angAccel * delta
 
 func Move(pos, _velocity, delta, returnNewPos=false, force=false):
-	_velocity.y += GRAV * delta * c
-	
-	var drag_force = airRes * _velocity.length_squared() * linDamp
-	_velocity -= _velocity.normalized() * drag_force * delta
-	
-	return _velocity
+    _velocity.y += GRAV * delta * c
+    
+    var drag_force = airRes * _velocity.length_squared() * linDamp
+    _velocity -= _velocity.normalized() * drag_force * delta
+    
+    return _velocity
 
 func _physics_process(delta):
-	if Active:
-		current_time += delta
-		
-		# Get delta velocity change for current time
-		var delta_velocity_at_time = get_delta_velocity_at_time(current_time)
-		var magnus_force_at_time = get_magnus_force_at_time(current_time) * current_time
-		if delta_velocity_at_time:
-			# Update the velocity based on the change
-			velocity -= delta_velocity_at_time * time_step 
-			
-			# Move the object
-			global_transform.origin += (velocity + magnus_force_at_time) * delta
-			lastPos = global_transform.origin
-			Spin(delta)
-			
-			# Debug output
-			print("Time: ", current_time, " Velocity: ", velocity, " Position: ", global_transform.origin)
-			
-			# Optionally: Print Magnus force for debugging
-			
-			print("Magnus Force at Time ", current_time, ": ", magnus_force_at_time)
-		
-		# Optional: Stop simulation after the lifetime ends
-		if current_time > max_time:
-			Active = false
+    if Active:
+        current_time += delta
+        
+        # Get delta velocity change for current time
+        var delta_velocity_at_time = get_delta_velocity_at_time(current_time)
+        var magnus_force_at_time = get_magnus_force_at_time(current_time) * current_time
+        if delta_velocity_at_time:
+            # Update the velocity based on the change
+            velocity -= delta_velocity_at_time * time_step 
+            
+            # Move the object
+            global_transform.origin += (velocity + magnus_force_at_time) * delta
+            lastPos = global_transform.origin
+            Spin(delta)
+            
+            # Debug output
+            print("Time: ", current_time, " Velocity: ", velocity, " Position: ", global_transform.origin)
+            
+            # Optionally: Print Magnus force for debugging
+            
+            print("Magnus Force at Time ", current_time, ": ", magnus_force_at_time)
+        
+        # Optional: Stop simulation after the lifetime ends
+        if current_time > max_time:
+            Active = false
