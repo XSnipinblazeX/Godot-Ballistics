5480137255ea39a2fc59db62e74975e4600d470f
XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>
2024-08-04 08:32:57 -0700

    Update Armor.gd
    Signed-off-by: XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>

--- Ballistics/Armor.gd
+++ Ballistics/Armor.gd
@@ -3,7 +3,8 @@
 extends StaticBody3D
 
 # Property to define the armor plate's penetration resistance
-@export var penetration_resistance = 10.0
+@export var penetration_resistance = [38.10, 38.1, 101.6]
+@export var constructiveArmorAngle = [0, 47, 47]
 @export var yield_strength = 1.5e9
 # Method to check if the node is an armor plate
 func is_armor_plate() -> bool:
@@ -11,6 +12,12 @@ func is_armor_plate() -> bool:
 
 # Method to get the penetration resistance
 func get_penetration_resistance(theta) -> float:
-	return abs(penetration_resistance / cos(theta))
+	
+	var armorThickness = 0
+	var index = 0
+	for armor in penetration_resistance:
+		armorThickness += abs(abs(armor / cos(theta)) / cos(constructiveArmorAngle[index])) # effective thickness for all armor plates
+		index += 1
+	return armorThickness
 func get_yield_strength() -> float:
 	return yield_strength
