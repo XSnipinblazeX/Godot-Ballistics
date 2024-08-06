caa6d87146cd5475010da136a256eecf75889f31
XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>
2024-08-06 14:49:06 -0700

    Update Armor.gd
    Signed-off-by: XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>

--- Ballistics/Armor.gd
+++ Ballistics/Armor.gd
@@ -1,21 +1,42 @@
 #this is the armor code, we calculate effective armor inside this script to hopefully make it simpler and take a load off of the other scripts
 
-#now takes into account spaced armor. The constructive angle is relative to the world space angle of the plate
-
-#default values are t26e4(WOT) or t26e1-1(WT) super pershing UFP 
-
 extends StaticBody3D
 
 # Property to define the armor plate's penetration resistance
 @export var penetration_resistance = [38.10, 38.1, 101.6]
 @export var constructiveArmorAngle = [3, -7, -14] # this is x plus the actual ingame rotation of the plate
 @export var distanceBetweenPlatesM = [0, 0.3, 0.3] # distance between plates in meters so we can find an exit point or determine if it even hits another plate in the array
-@export var yield_strength = 1.5e9
+@export var yield_strength = [3.5e8, 3.5e8, 4.5e8]
+#default values are for super pershing UFP
+#yield strength values:
+	# Steel: 3.5e8
+	# RHA: 4.5e8
+	# Aluminum: 2.4e8
+	# Iron: 2.562e8
+	# Hardwood: 6.0e7
+	# Softwood: 2.0e7
+	# Ballistic Glass: 1.2e8
+	# Normal Glass: 5.0e7
+	# Bone: 1.0e8
+	# Human: 3.0e7
+	# Plastic: 4.0e7
+	# Chobham Armor (estimate since its classified): 8.0e8
+	# Ballistic Ceramics: 6.0e8
+	# ice cream: 1000
+
+
+
 var exitPoint = Vector3.ZERO
 @export var damage_control_node = Node
 # Method to check if the node is an armor plate
 func is_armor_plate() -> bool:
 	return true
+
+func fuse_shell(fuseSensitivity, angle) -> bool:
+		if randi_range(0, 100) >= randi_range(0, 100000): # about 51 shells out of 100,000 will fail so its extremely uncommon but it exists..this excludes premature detonation
+			print("fuse failed")
+			return false 
+		return fuseSensitivity <= (penetration_resistance[0] / cos(angle))
 
 # Method to get the penetration resistance
 func get_penetration_resistance(theta) -> float:
@@ -35,4 +56,9 @@ func get_penetration_resistance(theta) -> float:
 	return armorThickness
 	print("distance traveled in armor array ", distanceTraveled)
 func get_yield_strength() -> float:
-	return yield_strength
+	var strength = 0
+	var index = 0
+	for armor in penetration_resistance:
+		strength += yield_strength[index]
+		index += 1
+	return (strength / yield_strength.size())
