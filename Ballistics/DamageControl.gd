31156ec9aa86b11e9cd075be2b7acb33e9e9f8ca
XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>
2024-08-06 16:52:22 -0700

    Update DamageControl.gd
    Signed-off-by: XSnipinblazeX <91927524+XSnipinblazeX@users.noreply.github.com>

--- Ballistics/DamageControl.gd
+++ Ballistics/DamageControl.gd
@@ -28,9 +28,48 @@ func is_within_cone(module_position, exit_vector, exit_position, cone_angle):
 	print(angle - 90)
 	return abs(angle - 90) <= (cone_angle / 2)
 	
+func calculate_tnt_equivalent(mass: float, re_factor: float = 1.0) -> float:
+	return mass * re_factor
+
+# Function to calculate peak pressure
+func calculate_peak_pressure(tnt_mass: float, scaling_factor: float = 100000) -> float:
+	return scaling_factor * tnt_mass
+
+# Function to calculate blast radius
+func calculate_blast_radius(peak_pressure: float, threshold_pressure: float = 5 * 1000) -> float:
+	print("blast radius 5kPa threshold: ", sqrt(peak_pressure / threshold_pressure) / 2)
+	return sqrt(peak_pressure / threshold_pressure) / 2
+
+# Function to calculate pressure at a given distance
+func pressure_at_distance(peak_pressure: float, distance: float, blast_radius: float) -> float:
+	if distance * 0.8 <= blast_radius:
+		return peak_pressure / pow(distance, 2)
+	else:
+		return 0
+
+	
+# Function to calculate and return pressure based on inputs
+func get_pressure(tnt_mass: float, distance: float) -> float:
+	var tnt_equiv = calculate_tnt_equivalent(tnt_mass)
+	var peak_pressure = calculate_peak_pressure(tnt_equiv)
+	var blast_radius = calculate_blast_radius(peak_pressure)
+	var pressure = pressure_at_distance(peak_pressure, distance, blast_radius)
+	return pressure
 	
 	
+func apply_explosion_damage(explodePos, tnt):
+	var index = 0
+	for module in Modules:
+		var module_position = get_node(module).transform.origin
+		var distance = module_position.distance_to(explodePos)
+		var pres = get_pressure(tnt, distance)
+		print(module, " received ", pres / 1000, " kPa")
+		mdHP[index] -= pres * 0.0005
+		if(mdHP[index] <= 0):
+			print(module, " was destroyed by explosion")
+		index += 1
 func apply_fragment_damage(exit_position, exit_vector, cone_angle, base_damage, fragmentDensity):
+	var index = 0
 	for module in Modules:
 		var module_position = get_node(module).transform.origin
 		var distance = module_position.distance_to(exit_position) * 1000
@@ -39,6 +78,10 @@ func apply_fragment_damage(exit_position, exit_vector, cone_angle, base_damage, 
 			print("new fragment density ", den)
 			var damage = calculate_damage(module_position, distance, base_damage, den)
 			print("module ", module, " experienced ", damage, " units of damage")
+			mdHP[index] -= damage
+			if(mdHP[index] < 0):
+				print("module ", module, " is desroyed")
+		index += 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
